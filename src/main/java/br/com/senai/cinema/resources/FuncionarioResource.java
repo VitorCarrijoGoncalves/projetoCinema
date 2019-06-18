9b8ce88d4b68a44b8f0f825855eec91d6bac3766
raonedaniel <raonedaniel2008@gmail.com>
2019-06-18 20:01:57 -0300

    Implementação das classes de serviço e controlador relacionadasa entidade funcionario

--- src/main/java/br/com/senai/cinema/resources/FuncionarioResource.java
+++ src/main/java/br/com/senai/cinema/resources/FuncionarioResource.java
@@ -1,5 +1,37 @@
 package br.com.senai.cinema.resources;
 
-public class FuncionarioResource {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.com.senai.cinema.services.FuncionarioService;
+
+@Controller
+//@RequestMapping(value="/login")
+public class FuncionarioResource {
+	
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	@PostMapping("/login")
+	public String confereLogin(HttpServletRequest request, HttpServletResponse response) {
+		
+		String email = request.getParameter("email");
+		String senha = request.getParameter("senha");
+		Object x = request.getParameter("g-recaptcha-response");
+				
+		if(!funcionarioService.validarLogin(email, senha)) {
+			request.setAttribute("erro", "Cadastro inexistente");
+			return "/login?m=cadastro-inexistente";
+		} else {
+			return "/home";
+		}
+		
+		
+		
+	}
+	
 }
--- src/main/java/br/com/senai/cinema/services/FuncionarioService.java
+++ src/main/java/br/com/senai/cinema/services/FuncionarioService.java
@@ -1,5 +1,44 @@
 package br.com.senai.cinema.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.senai.cinema.models.Funcionario;
+import br.com.senai.cinema.repositories.FuncionarioRepository;
+
+@Service
 public class FuncionarioService {
+	
+	@Autowired
+	private FuncionarioRepository funcionarioRepository;
+	
+	public boolean validarLogin(String email, String senha) {
+		
+		if(existeFuncionario(email)) {
+			Funcionario funcionario = findByEmail(email);
+			if(funcionario.getSenha().equals(senha)) {
+				return true;
+			}else {
+				return false;
+			}
+		}
+		return false;
+	}
+	
+	public Funcionario findByEmail(String email) {
+		Funcionario funcionario = funcionarioRepository.findByEmail(email);
+		if (funcionario == null) {
+			return null;
+		}
+		return funcionario;
+	}
+	
+	public boolean existeFuncionario(String email) {
+		if(findByEmail(email) !=null ) {
+			return true;
+		}
+		return false;
+		
+	}
 
 }
